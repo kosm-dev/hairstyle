2d54e31e398620293dd0b8f2ecafa2683a60733e
kosm-dev <72253360+kosm-dev@users.noreply.github.com>
2023-03-28 01:27:29 +0300

    added header and header adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,26 @@
-$(function () {
+window.addEventListener("DOMContentLoaded", () => {
+  const menuOpen = document.querySelector('.menu-open'),
+    menu = document.querySelector('.header__menu'),
+    body = document.querySelector('body'),
+    header = document.querySelector('header'),
+    menuClose = document.querySelector('.menu-close'),
+    logo = document.querySelector('.logo');
 
+  menuOpen.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    body.classList.toggle('lock');
+    header.classList.toggle('active');
+    menuOpen.classList.add('active');
+    menuClose.classList.add('active');
+    logo.classList.add('active');
+  });
+
+  menuClose.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    body.classList.toggle('lock');
+    header.classList.toggle('active');
+    menuOpen.classList.remove('active');
+    menuClose.classList.remove('active');
+    logo.classList.remove('active');
+  });
 });
